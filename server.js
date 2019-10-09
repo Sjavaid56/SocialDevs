06004c438eeb4583071cf01ddf9546c31952a0a8
sjavaid56 <shawnjavaid@gmail.com>
2019-10-09 19:30:09 +0100

    okay this pushed

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API running");
+    res.send("API running");
 });
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+okay this pushed
