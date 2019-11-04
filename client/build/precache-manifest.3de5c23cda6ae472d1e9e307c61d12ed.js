e211fdcf2e5069e44da8ebb44b28786fb7fa0967
sjavaid56 <shawnjavaid@gmail.com>
2019-11-04 16:09:23 +0000

    ready for deployment

---
+++ client/build/precache-manifest.3de5c23cda6ae472d1e9e307c61d12ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7530bebc45a46b18996e7a9581cfe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd370a8eb8b3225195c3",
+    "url": "/static/css/main.39d36049.chunk.css"
+  },
+  {
+    "revision": "afa1006f5fa9f129afb4",
+    "url": "/static/js/2.6136c005.chunk.js"
+  },
+  {
+    "revision": "dd370a8eb8b3225195c3",
+    "url": "/static/js/main.b0d1608a.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "421618453a958b3b5365cf129e47b798",
+    "url": "/static/media/computer-hands-laptop-2115217.42161845.jpg"
+  }
+]);
